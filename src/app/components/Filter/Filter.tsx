a72ea0a066e8fb99ce55a12ce836b61c91f7a890
knoedel44 <knoedel44@gmx.de>
2023-10-25 14:19:49 +0200

    adding list items for filter component

--- src/app/components/Filter/Filter.tsx
+++ src/app/components/Filter/Filter.tsx
@@ -1,9 +1,19 @@
 import styles from "./Filter.module.css";
+
 export default function Filter() {
     return (
-        <main>
-            <div className="filterContent">
-                <h1 className={styles.logo}>LOGO</h1>
+        <main className={styles.filterContainer}>
+            <div className={styles.logoContainer}>
+                <h1 className={styles.logo}><strong>HORIZON</strong> CARS</h1>
+            </div>
+            <div className={styles.filterCategoryContainer}>
+                <ul className={styles.filterCategories}>
+                    <li className={styles.filterItem}>Dashboard</li>
+                    <li className={styles.filterItem}>Orders</li>
+                    <li className={styles.filterItem}>Cars</li>
+                    <li className={styles.filterItem}>Customers</li>
+                    <li className={styles.filterItem}>Reports</li>
+                </ul>
             </div>
         </main>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Filter from "@/app/components/Filter/Filter";
+import styles from "./page.module.css";
+import DashboardContent from "@/app/components/Dashboard/overview/DashboardContent";
+
 export default function Home() {
-  return (
-      <div className="home">
-            <Filter />
-      </div>
-  )
+    return (
+        <div className={styles.pageContent}>
+            <Filter/>
+            <DashboardContent/>
+        </div>
+    )
 }
