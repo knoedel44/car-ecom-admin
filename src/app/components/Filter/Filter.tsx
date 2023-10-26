e282579fb56babada4a4815bd99594581dd90737
knoedel44 <knoedel44@gmx.de>
2023-10-26 11:26:10 +0200

    adding active effect on which list item was clicked

--- src/app/components/Filter/Filter.tsx
+++ src/app/components/Filter/Filter.tsx
@@ -1,20 +1,66 @@
+'use client'
+
+import React, { useState } from "react";
 import styles from "./Filter.module.css";
 
 export default function Filter() {
+    const [activeItem, setActiveItem] = useState("Dashboard");
+
+    const handleItemClick = (category) => {
+        setActiveItem(category);
+    };
+
     return (
         <main className={styles.filterContainer}>
             <div className={styles.logoContainer}>
-                <h1 className={styles.logo}><strong>HORIZON</strong> CARS</h1>
+                <h1 className={styles.logo}>
+                    <strong>HORIZON</strong> CARS
+                </h1>
             </div>
             <div className={styles.filterCategoryContainer}>
                 <ul className={styles.filterCategories}>
-                    <li className={styles.filterItem}>Dashboard</li>
-                    <li className={styles.filterItem}>Orders</li>
-                    <li className={styles.filterItem}>Cars</li>
-                    <li className={styles.filterItem}>Customers</li>
-                    <li className={styles.filterItem}>Reports</li>
+                    <li
+                        className={`${styles.filterItem} ${
+                            activeItem === "Dashboard" ? styles.active : ""
+                        }`}
+                        onClick={() => handleItemClick("Dashboard")}
+                    >
+                        Dashboard
+                    </li>
+                    <li
+                        className={`${styles.filterItem} ${
+                            activeItem === "Orders" ? styles.active : ""
+                        }`}
+                        onClick={() => handleItemClick("Orders")}
+                    >
+                        Orders
+                    </li>
+                    <li
+                        className={`${styles.filterItem} ${
+                            activeItem === "Cars" ? styles.active : ""
+                        }`}
+                        onClick={() => handleItemClick("Cars")}
+                    >
+                        Cars
+                    </li>
+                    <li
+                        className={`${styles.filterItem} ${
+                            activeItem === "Customers" ? styles.active : ""
+                        }`}
+                        onClick={() => handleItemClick("Customers")}
+                    >
+                        Customers
+                    </li>
+                    <li
+                        className={`${styles.filterItem} ${
+                            activeItem === "Reports" ? styles.active : ""
+                        }`}
+                        onClick={() => handleItemClick("Reports")}
+                    >
+                        Reports
+                    </li>
                 </ul>
             </div>
         </main>
-    )
+    );
 }
