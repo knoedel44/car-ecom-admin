5bfa15ac89725a8125f50ade7fb1248043799e35
knoedel44 <knoedel44@gmx.de>
2023-10-30 10:48:55 +0100

    adding nextauth.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Filter from "@/app/components/Layout/Filter";
-import styles from "./page.module.css";
 import DashboardContent from "@/app/components/Dashboard/overview/DashboardContent";
 
 export default function Home() {
