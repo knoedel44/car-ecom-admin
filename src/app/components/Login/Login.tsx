37a15e431041e0cf73d65e3c9e9a361859b69041
knoedel44 <knoedel44@gmx.de>
2023-11-13 21:26:05 +0100

    adjusting login components

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Login() {
     const { data: session } = useSession();
